4639f9fe6e90a13148ba31c8db1665f21a1292ac
Guilherme <guilhermesouzaoliveira0308@gmail.com>
2026-05-08 22:25:49 -0300

    Alteração api

--- src/main/java/br/belval/api/controller/ProdutoController.java
+++ src/main/java/br/belval/api/controller/ProdutoController.java
@@ -1,12 +1,16 @@
 package br.belval.api.controller;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,10 +29,26 @@ public class ProdutoController {
 	 */
 	@GetMapping("/produtos")
 	public ResponseEntity<Iterable<Produto>> obterProdutos() {
-		return ResponseEntity
+ 		return ResponseEntity
 			.status(HttpStatus.OK)
 			.body(repository.findAll());
 	}
+	
+	@GetMapping("/produtos/{id}")
+	public ResponseEntity<Object> buscarPorId(
+			@PathVariable(value = "id") Integer id) {
+		
+		Optional<Produto> produtoOpt = repository.findById(id);
+		
+		if (produtoOpt.isPresent()) {
+			return ResponseEntity
+					.status(HttpStatus.OK)
+					.body(produtoOpt.get());
+		}
+		return ResponseEntity
+				.status(HttpStatus.NOT_FOUND)
+				.body("Produto não encontrado!");
+	}	
 	
 	//curl POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
 	@PostMapping("/produtos")
@@ -43,6 +63,51 @@ public class ProdutoController {
 		return ResponseEntity
 				.status(HttpStatus.CREATED)
 				.body(produto);
+	}	
+	
+	//curl POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
+	@PutMapping("/produtos")
+	public ResponseEntity<Object> atualizarProduto(
+			@PathVariable Integer id,
+			@RequestBody Produto produto) {
+		
+         Optional<Produto> produtoOpt = repository.findById(id);
+         
+         if (produtoOpt.isEmpty()) {
+        	 return ResponseEntity
+        			 .status(HttpStatus.NOT_FOUND)
+        			 .body("Produto não encontrado!");
+         }
+          
+         produto.setId(id);
+         produto.setDataCriacao(produtoOpt.get().getDataCriacao());
+         
+         repository.save(produto);
+         
+         return ResponseEntity
+        		 .status(HttpStatus.OK)
+        		 .body("Produto atualizado com sucesso!");
+
 	}
 
+@DeleteMapping("/produtos/{id}")
+public ResponseEntity<String> apagarProduto(@PathVariable Integer id) {
+
+    Optional<Produto> produtoOpt = repository.findById(id);
+     
+    if (produtoOpt.isEmpty()) {
+    	return ResponseEntity
+    			.status(HttpStatus.NOT_FOUND)
+    			.body("Produto não encontrado!");
+    }
+    
+    repository.deleteById(id);
+    
+    return ResponseEntity
+    		.status(HttpStatus.OK)
+    		.body("Produto apagado com sucesso!");
+	
+
+
+}
 }
